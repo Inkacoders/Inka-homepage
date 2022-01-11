362876881c60f3ba00ea27d65535337b30a2c34c
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2022-01-11 14:59:16 -0500

    Grid gallery mobile design created

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import "../styles/form.css";
 import "../styles/header.css";
 import "../styles/burger-btn.css";
 import "../styles/menu.css";
+import "../styles/grid-gallery.css";
 
 import { $burgerBtn } from "./selectors.js";
 import { menuHandle } from "./functions.js";
