29ae00f12f4b4423b56f1d4434e866bb618c46cc
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2022-01-12 20:29:13 -0500

    Card component created

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import "../styles/main.css";
+import "../styles/icon.css";
 import "../styles/wrapper.css";
 import "../styles/home.css";
 import "../styles/form.css";
@@ -7,6 +8,7 @@ import "../styles/burger-btn.css";
 import "../styles/menu.css";
 import "../styles/grid-gallery.css";
 import "../styles/community.css";
+import "../styles/card.css";
 
 import { $burgerBtn } from "./selectors.js";
 import { menuHandle } from "./functions.js";
