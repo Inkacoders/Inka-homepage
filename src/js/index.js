ccf0df7cab107ecc6b130bf4876451d7086390a5
Jefferson Araujo <jeffersonaraujo491@gmail.com>
2022-04-05 16:54:37 -0500

    ccs files path fixed

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,19 @@
-import "../styles/settings/fonts.css"
-import "../styles/settings/variables.css"
-
-import "../styles/elements/body.css"
-import "../styles/elements/icon.css"
-import "../styles/elements/wrapper.css"
-
-import "../styles/objects/burger-btn.css"
-
+import "../styles/components/burger-btn.css"
 import "../styles/components/card.css";
 import "../styles/components/community.css";
 import "../styles/components/form.css";
 import "../styles/components/grid-gallery.css";
 import "../styles/components/header.css";
 import "../styles/components/home.css";
+import "../styles/components/icon.css";
 import "../styles/components/menu.css";
 import "../styles/components/portfolio.css";
 import "../styles/components/services.css";
 import "../styles/components/testimonial.css";
 
 import "../styles/elements/body.css";
-import "../styles/elements/icon.css";
-import "../styles/elements/wrapper.css";
 
-import "../styles/objects/burger-btn.css";
+import "../styles/objects/wrapper.css";
 
 import "../styles/settings/fonts.css";
 import "../styles/settings/variables.css";
