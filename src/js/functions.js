46d8c3c4411da79c52611855c0b88062811a0aa9
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2022-04-05 19:33:52 -0500

    Menu resets based on the device

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,20 +1,30 @@
 import { $burgerBtn, $menuContainer, $body } from "./selectors.js";
 
 function menuHandle() {
-    const isOpen = $burgerBtn.classList.toggle("burger-btn--pressed");
+  const isOpen = $burgerBtn.classList.toggle("burger-btn--pressed");
 
-    if (isOpen) {
-        $menuContainer.classList.add("menu__container--expanded");
-        $menuContainer.ariaExpanded = "true";
-        $burgerBtn.ariaPressed = "true";
-        $body.style.overflowY = "hidden";
-        return;
-    }
+  if (isOpen) {
+    $menuContainer.classList.add("menu__container--expanded");
+    $menuContainer.ariaExpanded = "true";
+    $burgerBtn.ariaPressed = "true";
+    $body.style.overflowY = "hidden";
+    return;
+  }
 
+  $menuContainer.classList.remove("menu__container--expanded");
+  $menuContainer.ariaExpanded = "false";
+  $burgerBtn.ariaPressed = "false";
+  $body.style.overflowY = "auto";
+}
+
+function resetMenu() {
+  if ($burgerBtn.classList.contains("burger-btn--pressed")) {
+    $burgerBtn.classList.remove("burger-btn--pressed");
     $menuContainer.classList.remove("menu__container--expanded");
     $menuContainer.ariaExpanded = "false";
     $burgerBtn.ariaPressed = "false";
     $body.style.overflowY = "auto";
+  }
 }
 
-export { menuHandle };
+export { menuHandle, resetMenu };
