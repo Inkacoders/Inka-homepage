3545bbde04ea7e5fe4865830635ee28803ce5f31
Jefferson Araujo <jeffersonaraujo491@gmail.com>
2022-04-18 20:58:45 -0500

    Updates

--- src/js/selectors.js
+++
@@ -1,3 +0,0 @@
-export const $body = document.querySelector(".body");
-export const $burgerBtn = document.querySelector(".burger-btn");
-export const $menuContainer = document.querySelector(".menu");
